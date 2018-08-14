45d6cf9bc98ac22510258cd853369069044eacb6
Amareshwar2437 <42177109+Amareshwar2437@users.noreply.github.com>
2018-08-14 10:20:39 +0000

    Add files via upload

---
+++ Amareshwar.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+echo "What is your name?"
+
+read x
+
+mkdir $x
+
+cd $x
+
+touch $x.cpp
+
+#!bin/bash
+echo "What is your name?"
+
+read x
+
+mkdir $x
+
+cd $x
+
+for i in 1 2 3 4 5
+
+      do
+         touch $x$i.cpp
+      done
+
+
+
+
