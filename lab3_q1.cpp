461389e5fa3d6eb912f60e0d4371a7c81744bbd5
Amareshwar2437 <42177109+Amareshwar2437@users.noreply.github.com>
2018-08-16 15:29:45 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,49 @@
+// Library
+#include <iostream>
+using namespace std;
+
+// To declare the main function
+int main() {
+
+           // Declaring variables of all types
+
+           int i;
+
+           float a;
+
+           double b;
+
+           char c;
+
+           bool d;
+// Now we have to initialize each one of them
+
+	i=24;
+	a=6.7;
+	b=3.145;
+	c='X';
+	d=true;
+// Now we have to get output of all the values
+
+	cout << i << endl;
+
+	cout << a << endl;
+
+	cout << b << endl;
+
+	cout << c << endl;
+
+	cout << d << endl;
+//Now we display the size of all variables
+	cout<<"Size of i ="<<sizeof(i)<<endl;
+
+	cout<<"Size of a ="<<sizeof(a)<<endl;
+
+	cout<<"Size of b ="<<sizeof(b)<<endl;
+
+	cout<<"Size of c ="<<sizeof(c)<<endl;
+
+	cout<<"Size of d ="<<sizeof(d)<<endl;
+return 0;
+}
+	
---
+++ lab3_q2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+               //Now declaring the variables
+	float x,y;
+	// Making initialisation of the variables
+	x=43;
+	y=97;
+
+	cout<<" First & Second Variables so declared are "<<x<<" and "<<y<< endl;
+
+//Carrying out the respective functions and operations
+
+	cout<<"The sum is"<<x+y<<endl;
+
+	cout<<"The difference is"<<y-x<<endl;
+
+	cout<<"The product is"<<x*y<<endl;
+
+	cout<<"Their division is"<<y/x<<endl;
+
+	int a=42,b=11;
+
+	cout<<"The modulus is"<<a%b<<endl;
+return 0;
+
+}
